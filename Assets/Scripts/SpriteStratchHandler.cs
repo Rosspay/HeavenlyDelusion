74a55b4988e2d3c1d632892133d775110eb9d824
Rosspay <lodygin@sfedu.ru>
2024-12-23 18:57:51 +0300

    Main menu + other stuff

---
+++ Assets/Scripts/SpriteStratchHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteStratchHandler : MonoBehaviour
+{
+    public bool isAspectRatio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
